2d81c01f9ffbcc55bed0d0989708ea5fd19b509a
= <=>
2024-01-09 22:28:45 +0100

    basic robots.txt added

---
+++ app/robots.js
@@ -0,0 +1,12 @@
+import { CLIENT_URL } from "@/constants";
+
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
+    },
+    sitemap: `${CLIENT_URL}/sitemap.xml`,
+  };
+}

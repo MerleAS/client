b48963b10211ca153985838d817be549fe58770f
= <=>
2024-01-16 12:11:50 +0100

    .

--- actions/getProducts.js
+++ actions/getProducts.js
@@ -2,22 +2,21 @@
 
 export const getProducts = async () => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/products/get-products`, {
-      headers: {
-        'Cache-Control': 'no-store' // Disables caching for this request
-      }
-    });
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/products/get-products`,
+      { cache: 'no-store' },
+    )
 
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error('Network response was not ok')
     }
 
-    const data = await response.json();
-    return data.products;
+    const data = await response.json()
+    return data.products
   } catch (error) {
-    console.error('There was a problem with the fetch operation:', error);
+    console.error('There was a problem with the fetch operation:', error)
   }
-};
+}
 
 /* import axios from 'axios'
 

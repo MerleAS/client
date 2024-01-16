5491cc228e3d38f4faa2ffcdc924fd40f3c6596a
= <=>
2024-01-16 11:45:29 +0100

    .

--- actions/getProducts.js
+++ actions/getProducts.js
@@ -1,13 +1,15 @@
-import axios from "axios";
-import { cache } from "react";
+'use server'
+
+import axios from 'axios'
+import { cache } from 'react'
 
 export const getProducts = cache(async () => {
   try {
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_SERVER_URL}/products/get-products`
-    );
-    return response.data.products;
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/products/get-products`,
+    )
+    return response.data.products
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-});
+})

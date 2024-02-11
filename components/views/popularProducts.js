ab15ab48d49c0de9f44fca714369aad2f7333cb6
= <=>
2024-02-11 09:25:01 +0100

    popular products fixed

--- components/views/popularProducts.js
+++ components/views/popularProducts.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRef } from 'react'
+import { useRef, useCallback } from 'react'
 import useIsMobile from '../../util/useIsMobile'
 
 import Image from 'next/image'
@@ -13,16 +13,29 @@ import 'swiper/css/pagination'
 import 'swiper/css/navigation'
 
 const PopularProducts = ({ products, containerClass }) => {
-  const prevRef = useRef(null)
-  const nextRef = useRef(null)
+  const sliderRef = useRef(null)
+
+  const handlePrev = useCallback(() => {
+    if (!sliderRef.current) return
+    sliderRef.current.swiper.slidePrev()
+  }, [])
+
+  const handleNext = useCallback(() => {
+    if (!sliderRef.current) return
+    sliderRef.current.swiper.slideNext()
+  }, [])
+
   const width = useIsMobile('dynamic')
 
   const modules = [A11y]
 
   if (width > 800) modules.push(Navigation)
+
   return (
     <div
-      className={`flex flex-col md:px-6 space-y-12 w-full max-w-[100vw] py-12 bg-dark/5 ${width < 800 ? "px-8" : ""}`}
+      className={`flex flex-col md:px-6 space-y-12 w-full max-w-[100vw] py-12 bg-dark/5 ${
+        width < 800 ? 'px-8' : ''
+      }`}
     >
       <h4 className="w-full flex items-center justify-center font-medium text-lg md:text-2xl">
         Populære Produkter
@@ -36,21 +49,14 @@ const PopularProducts = ({ products, containerClass }) => {
               position: 'relative',
               marginRight: width > 800 ? '32px' : '22px',
             }}
-            ref={prevRef}
+            onClick={handlePrev}
           />
         )}
 
         <Swiper
           slidesPerView={width > 1100 ? 3 : width > 800 ? 2 : 1}
           modules={modules}
-          navigation={{
-            prevEl: prevRef.current,
-            nextEl: nextRef.current,
-          }}
-          onBeforeInit={(swiper) => {
-            swiper.params.navigation.prevEl = prevRef.current
-            swiper.params.navigation.nextEl = nextRef.current
-          }}
+          ref={sliderRef}
         >
           {products.map((prod, idx) => {
             return (
@@ -87,7 +93,7 @@ const PopularProducts = ({ products, containerClass }) => {
         {width > 800 && (
           <span
             className="swiper-button-next"
-            ref={nextRef}
+            onClick={handleNext}
             style={{
               position: 'relative',
               marginLeft: width > 800 ? '32px' : '22px',
@@ -100,54 +106,3 @@ const PopularProducts = ({ products, containerClass }) => {
 }
 
 export default PopularProducts
-/* 'use client'
-
-import Image from 'next/image'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { A11y, Navigation } from 'swiper/modules'
-
-import "../../public/styles/swiper.css";
-import 'swiper/css'
-import 'swiper/css/pagination'
-import 'swiper/css/navigation'
-
-const PopularProducts = ({ products }) => {
-  console.log('prods', products)
-
-  return (
-    <div className="hidden md:flex px-12">
-      <Swiper
-        slidesPerView={3}
-        modules={[A11y, Navigation]}
-        navigation={true}
-      >
-        {products.map((prod, idx) => {
-          return (
-            <SwiperSlide key={idx} className="w-full h-full px-2 space-y-2">
-              <Image
-                className=""
-                id="container"
-                loader={() =>
-                  `${process.env.NEXT_PUBLIC_SERVER_URL}/${prod.imageUrls[0]}`
-                }
-                src={`${process.env.NEXT_PUBLIC_SERVER_URL}/${prod.imageUrls[0]}`}
-                alt="image"
-                width={1000}
-                height={1500}
-                objectFit="cover"
-                draggable={false}
-              />
-              <p className="font-medium text-lg">{prod.title}</p>
-              <p className="text-dark opacity-70">{prod.description}</p>
-              <p className="font-semibold">{prod.price} kr</p>
-            </SwiperSlide>
-          )
-        })}
-      </Swiper>
-    </div>
-    </>
-  )
-}
-
-export default PopularProducts
- */
